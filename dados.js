690ff49e4403a4ddad79cb0e1b15bf552baa6e5d
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-02-22 20:46:56 -0300

    Atualizar o dados.js

---
+++ dados.js
@@ -0,0 +1,38 @@
+var nome = localStorage.getItem("nome")
+
+var acessando = window.location.href;
+var data = new Date()
+var mes = data.toLocaleString('pt-BR', { month: 'long' });
+console.log(mes);
+var hora = data.getHours()+":"+data.getMinutes()
+var dia = data.getDate() +"/"+mes+"/"
++data.getFullYear()+" Hora: "+hora
+
+
+if (nome === null) {
+
+ nome = prompt("Digite seu Nome")
+localStorage.setItem("nome",nome)
+
+}
+
+fetch('https://api.sheetmonkey.io/form/pQQpZrU83ib6ocCX93j2oS', {
+    
+    method: 'post',
+    headers: {
+      
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+      
+    },
+    body: JSON.stringify({
+      
+      Usuário: nome,
+      acessando: acessando,
+      Data: dia
+      
+      
+      
+    })
+   
+  })
